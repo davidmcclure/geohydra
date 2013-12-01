8806de0095dee9739fc08b7c90b8d270645c6576
David McClure <david.mcclure@virginia.edu>
2013-12-01 11:01:34 -0800

    Adds geohydra.tmpdir configuration option to example Confstruct.

--- config/environments/example.rb
+++ config/environments/example.rb
@@ -3,12 +3,13 @@ module GeoHydra
     geohydra do
       workspace "/var/geomdtk/current/workspace"
       stage "/var/geomdtk/current/stage"
+      tmpdir "/var/geomdrk/current/tmp"
     end
 
     geonetwork do
       service_root 'http://host/geonetwork'
     end
-    
+
     geoserver do
       workspace 'druid'
       datastore 'geoserver'
@@ -30,12 +31,12 @@ module GeoHydra
         end
       end
     end
-    
+
     postgis do
       url 'postgres://geostaff:@localhost:5432/geoserver'
       schema 'myschema'
     end
-    
+
     ogp do
       geoserver 'http://host/geoserver'
       purl 'http://host'
@@ -44,7 +45,7 @@ module GeoHydra
         collection 'mycollection'
       end
     end
-    
+
   end
 end
 
@@ -56,10 +57,10 @@ Dor::Config.configure do
     sleep_time   1
   end
 
-  fedora do 
+  fedora do
     url 'https://user:mypassowrd@host/fedora'
   end
-  
+
   gsearch do
     url 'http://user:mypassword@host/solr'
   end
@@ -67,7 +68,7 @@ Dor::Config.configure do
   solrizer do
     url 'http://user:mypassword@host/solr'
   end
-  
+
   ssl do
     cert_file File.join(File.dirname(__FILE__) + '/../certs', "my.crt")
     key_file  File.join(File.dirname(__FILE__) + '/../certs', "my.key")
@@ -81,18 +82,18 @@ Dor::Config.configure do
     user 'myuser'
     pass 'mypassword'
   end
-  
+
   workflow do
     url 'http://user:mypassword@host/workflow/'
   end
-  
+
   content do
     content_user 'myuser'
     content_base_dir '/my/content'
     content_server 'my.content.host'
     ssh_auth 'gssapi-with-mic'
   end
-  
+
   stacks do
     document_cache_storage_root '/my/document/cache'
     document_cache_host 'my.document.cache.host'
